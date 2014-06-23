d9122a0cfd07123fa753ec492bb6ba81e744b4dc
kangdongkil <kbk3418@naver.com>
2014-06-23 15:55:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	$("#in").click(function() {
-		$(this).toggleClass("state1");
-		$(this).toggleClass("state2");
 
 		$("#InSound")[0].load();
 		$("#InSound")[0].play();
